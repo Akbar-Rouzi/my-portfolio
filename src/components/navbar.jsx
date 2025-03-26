b2816a9b873ac6064bf7ce1268f9ff51120e41b0
Akbar <seagull826@gmail.com>
2025-03-26 00:47:55 -0400

    Enable menu list toggle

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
@@ -6,6 +8,7 @@ const links = [
   { url: "/contact", title: "Contact" },
 ];
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LOGO */}
@@ -23,19 +26,24 @@ const Navbar = () => {
       {/* RESPONSIVE MENU */}
       <div className="">
         {/* MENU BUTTON */}
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative cursor-pointer">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
           <div className="w-10 h-1 bg-white rounded"></div>
         </button>
         {/* MENU LIST */}
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-          {links.map((link) => (
-            <Link href={link.url} key={link.title}>
-              {link.title}
-            </Link>
-          ))}
-        </div>
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
