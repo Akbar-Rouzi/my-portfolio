6ffd5b5a1209cb0f27e4e19ad8bff49112f1f36b
Akbar <seagull826@gmail.com>
2025-03-26 01:32:23 -0400

    Add social media icons and adjust items visibility based on screen size change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 const links = [
   { url: "/", title: "Home" },
@@ -10,9 +11,17 @@ const links = [
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* LINKS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <Link href={link.url} key={link.title}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+      <div className="md:hidden lg:flex w-1/3 justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -23,8 +32,20 @@ const Navbar = () => {
           </span>
         </Link>
       </div>
+      {/* SOCIALS */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="#">
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="#">
+          <Image src="/facebook.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
       {/* RESPONSIVE MENU */}
-      <div className="">
+      <div className="md:hidden">
         {/* MENU BUTTON */}
         <button
           className="w-10 h-8 flex flex-col justify-between z-50 relative cursor-pointer"
