bac9aedbe209d11df50db7e1524dfe8711278287
Akbar <seagull826@gmail.com>
2025-11-28 15:09:10 -0500

    update linkedin URL

--- src/utils/socials.js
+++ src/utils/socials.js
@@ -1,6 +1,6 @@
 const socials = [
 	{ name: 'github', src: '/github.png', url: 'https://github.com/Akbar-Rouzi' },
-	{ name: 'linkedin', src: '/linkedin.png', url: 'https://www.linkedin.com' }
+	{ name: 'linkedin', src: '/linkedin.png', url: 'https://www.linkedin.com/' }
 ];
 
 export default socials;
