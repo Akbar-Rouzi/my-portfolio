c497f31f65d16c77d6321693af8a55e8fbbe9706
Akbar <seagull826@gmail.com>
2025-11-28 14:28:26 -0500

    Update Summay

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import digital from "@/utils/digital";
 const Homepage = () => {
 	return (
 		<motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
-			<div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+			<div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 overflow-auto">
 				{/* IMAGE CONTAINER */}
 				<div className="h-1/2 lg:h-full lg:w-1/2 relative">
 					<Image
@@ -20,7 +20,7 @@ const Homepage = () => {
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
 				</div>
 				{/* TEXT CONTAINER */}
-				<div className="h-1/2 flex lg:h-full lg:w-1/2 flex-col gap-4 md:gap-6 lg:gap-8 items-center justify-center my-16 sm:my-0 ">
+				<div className="h-1/2 flex lg:h-full lg:w-1/2 flex-col gap-4 md:gap-6 lg:gap-8 items-center justify-center">
 					{/* TITLE */}
 					<h1 className="text-2xl md:text-4xl lg:text-6xl font-bold"> {digital.title}</h1>
 					{/* DESCRIPTION */}
--- src/utils/summaryData.js
+++ src/utils/summaryData.js
@@ -1,9 +1,26 @@
 const summaryData = {
 	title: 'SUMMARY',
 	desc: [
-		{ p: 'Versatile Front-End and Salesforce Developer with strong experience in building scalable, responsive web applications and CRM solutions. Proficient in front-end frameworks including React.js, Next.js, Angular, and Vue.js, and back-end technologies such as Node.js, Express.js, and RESTful API integration. Skilled in full-stack development and database handling with MongoDB and MySQL.', id: 'p1' },
-		{ p: 'As a Salesforce Full Stack Developer, experienced in Apex, Lightning Web Components (LWC), SOQL, and Salesforce platform automation. Proven ability to build secure, high-performance portals and apps that integrate seamlessly with Salesforce CRM.', id: 'p2' },
-		{ p: 'Open to Front-End, MERN Stack, or Salesforce Developer roles focused on delivering user-centric and business-driven solutions.', id: 'p3' },
+		{
+			p: 'Innovative and detail-oriented front-end developer skilled in building scalable, high-performance web applications    with modern frameworks and technologies',
+			id: 'p1'
+		},
+		{
+			p: 'Expert in JavaScript, TypeScript, React, Vue, Angular, HTML5, CSS3 and TailwindCSS, specializing in custom UI components, responsive design, cross-browser compatibility, and performance optimization.',
+			id: 'p2'
+		},
+		{
+			p: 'Experienced in Node.js and Express.js for backend development, API integration, and server-side logic implementation. Have a strong knowledge of JAVA.',
+			id: 'p3'
+		},
+		{
+			p:'Strong knowledge of Salesforce Administration, Apex, SOQL, DML, Trigger and LWC, with a background in workflow automation and customization',
+			id:'p4'
+		},
+		{
+			p:'Passionate about mentoring junior developers, collaborating with cross-functional teams, and driving innovation through clean, efficient, and maintainable code. Enthusiastic, always learning new technologies.',
+			id:'p5'
+		}
 	],
 	quote: 'Code is logic. UI is emotion. I blend both to tell digital stories.'
 }
